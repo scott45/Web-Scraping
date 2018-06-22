f9eace6e6da6c4348a7cc818bc418d51e0ebcbf8
scotty-b <businge.scott@andela.com>
2018-06-22 21:04:50 +0300

    add code

--- scraping/github.py
+++ scraping/github.py
@@ -6,3 +6,15 @@ from selenium.common.exceptions import TimeoutException
 
 option = webdriver.ChromeOptions()
 option.add_argument(“ — incognito”)
+
+browser = webdriver.Chrome(executable_path=’/Library/Application Support/Google/chromedriver’, chrome_options=option)
+
+browser.get(“https://github.com/scott45")
+
+# Wait 20 seconds for page to load
+timeout = 20
+try:
+    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, “//img[@class=’avatar width-full rounded-2']”)))
+except TimeoutException:
+    print(“Timed out waiting for page to load”)
+    browser.quit()
